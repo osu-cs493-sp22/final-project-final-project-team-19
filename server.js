5a9b8bc9d3b07b173f63708d3c8ff56d094246ef
Rory S Moeller <moellerr@oregonstate.edu>
2022-06-04 16:31:17 -0700

    Connect server to mongo db

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const { connect } = require('./lib/mongoose')
 
 const app = express()
 const port = process.env.PORT || 8000
 
-app.listen(port, () => {
-    console.log(`== Server is running on port ${port}`)
+connect(() => {
+    app.listen(port, () => {
+        console.log(`== Server is running on port ${port}`)
+    })
 })
