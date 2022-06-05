dff6411371fa5693dd87b695afdabf121c28cdd1
Ryan Zimmerman <zimmerry@oregonstate.edu>
2022-06-04 19:20:21 -0700

    Implement login and fetching user data

--- api/users.js
+++ api/users.js
@@ -1,8 +1,9 @@
+const bcrypt = require('bcryptjs')
 const { Router } = require('express')
 
 const router = Router()
 
-const { requireAuthentication } = require('../lib/auth')
+const { requireAuthentication, generateAuthToken } = require('../lib/auth')
 const { userSchema, User } = require('../models/user')
 
 // Create a new User
@@ -13,30 +14,38 @@ const { userSchema, User } = require('../models/user')
  * 'instructor' roles. 
  */
 router.post('/', async (req, res, next) => {
-    // TODO: add require authentication and check for admin role 
-    // w/ new admin & instructor users
+    // TODO: check for admin role w/ new admin & instructor users
 
     const newUser = new User(req.body)
     let error = newUser.validateSync();
-    if(error) {
-        res.status(400).send({
-            error: "Request body does not contain a valid User object"
-        })
-    } else {
-        // console.log(newUser)
+    
+    if(!error) {
         const users = await User.find({ email: newUser.email});
-        console.log(users)
+        // check if there's an existing user with the given email
         if (users.length > 0) {
             res.status(400).send({
                 error: "A user with that email already exists"
             })
-        } else {
+
+        } else if (newUser.role == "student") { // create student with no auth
             await newUser.save()
             console.log(users)
             res.status(201).send({
                 id: newUser._id
             })
+
+        } else { // TODO: require auth if creating instructor or admin
+            await newUser.save()
+            console.log(users)
+            res.status(201).send({
+                id: newUser._id
+            })
+
         }
+    } else {
+        res.status(400).send({
+            error: "Request body does not contain a valid User object"
+        })
     }
 })
 
@@ -48,6 +57,26 @@ router.post('/', async (req, res, next) => {
 router.post("/login", async (req, res, next) => {
     if (req.body && req.body.email && req.body.password ) {
 
+        // find the user with a matching email, if any
+        const user = await User.find({ email: req.body.email })
+
+        // check if the credentials are valid
+        const validCredentials = (user.length == 1) && await bcrypt.compare(
+            req.body.password,
+            user[0].password
+        )
+
+        if (validCredentials) {
+            const token = generateAuthToken(String(user[0]._id))
+            res.status(200).send({
+                token: token
+            })
+        
+        } else {
+            res.status(401).send({
+                error: "Invalid credentials"
+            })
+        }
     } else {
         res.status(400).send({
             error: "The request body must contain an email and password"
@@ -66,7 +95,36 @@ router.post("/login", async (req, res, next) => {
  *  authenticated User whose ID matches the ID of the requested
  *  User can fetch this information.
  */
-router.get(":userId", requireAuthentication, (req, res, next) => {
-    // TODO: Implement
+router.get("/:userId", requireAuthentication, async (req, res, next) => {
+    console.log(req.user)
+    const user = await User.find({ _id: req.params.userId })
+
+    if (user.length == 1) {
+        if(req.user.role == 'instructor') {
+            // TODO: add the courses the student is in that the instructor teaches
+            res.status(200).send({
+                user: user[0].name,
+                email: user[0].email,
+                password: user[0].password,
+                role: user[0].role
+            })
+        } else if (req.user._id == req.params.userId) {
+            // TODO: add the courses the student is in
+            res.status(200).send({
+                user: user[0].name,
+                email: user[0].email,
+                password: user[0].password,
+                role: user[0].role
+            })
+        } else {
+            res.status(403).send({
+                error: "You are not authorized to access this resource"
+            })
+        }
+    } else {
+        next()
+    }
+
+    
 })
 module.exports = router
